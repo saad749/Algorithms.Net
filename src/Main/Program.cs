e566bf6d85607cb3f2f17428371b074da0496f89
Saad Farooq <saad749@yahoo.com>
2017-09-22 21:50:26 +0300

    Migrated aalhour/C-Sharp-Algorithms to .net standard

---
+++ src/Main/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using C_Sharp_Algorithms.AlgorithmsTests;
+using C_Sharp_Algorithms.DataStructuresTests;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            OpenAddressingHashTableTest.DoTest();
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
